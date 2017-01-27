b28898c3b49ff4d11751899a3cc07be683869f49
Par Steffansson <par.steffansson@sent.com>
2017-01-28 05:49:49 +0700

    finished question 7

--- smartcab/smartcab/agent.py
+++ smartcab/smartcab/agent.py
@@ -23,7 +23,10 @@ class LearningAgent(Agent):
         ## TO DO ##
         ###########
         # Set any additional class parameters as needed
-
+        self.trial = 0
+        self.previous_state = None
+        self.previous_action = None
+        self.previous_reward = None
 
     def reset(self, destination=None, testing=False):
         """ The reset function is called at the beginning of each trial.
@@ -39,11 +42,15 @@ class LearningAgent(Agent):
         # Update epsilon using a decay function of your choice
         # Update additional class parameters as needed
         # If 'testing' is True, set epsilon and alpha to 0
+        self.previous_state = None
+
         if testing:
             self.epsilon = 0
             self.alpha = 0
         else:
-            self.epsilon = self.epsilon - 0.05
+            self.trial = self.trial + 1
+            #self.epsilon = self.epsilon - 0.05  # default sim
+            self.epsilon = math.pow(0.985, self.trial-1)
 
         return None
 
@@ -142,10 +149,16 @@ class LearningAgent(Agent):
         ###########
         # When learning, implement the value iteration update rule
         #   Use only the learning rate 'alpha' (do not use the discount factor 'gamma')
-        self.Q[state][action] = self.alpha * (reward + self.get_maxQ(state)) + (1.0-self.alpha) * self.Q[state][action]
+        if self.previous_state is not None:
+            self.Q[self.previous_state][self.previous_action] = self.alphaAdd(self.Q[self.previous_state][self.previous_action], self.previous_reward + self.get_maxQ(state))
 
+        self.previous_state = state
+        self.previous_action = action
+        self.previous_reward = reward
         return
 
+    def alphaAdd(self, old_value, new_value):
+        return (1.0 - self.alpha) * old_value + self.alpha * new_value
 
     def update(self):
         """ The update function is called when a time step is completed in the 
@@ -179,7 +192,7 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent, learning=True)
+    agent = env.create_agent(LearningAgent, learning=True, alpha=0.2)
     
     ##############
     # Follow the driving agent
@@ -194,14 +207,15 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, update_delay=0.01, log_metrics=True, display=False)
+    #sim = Simulator(env, update_delay=0.01, log_metrics=True, display=False, optimized=False) # default
+    sim = Simulator(env, update_delay=0.01, log_metrics=True, display=False, optimized=True) # optimized
     
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test=10)
+    sim.run(n_test=5, tolerance=0.001)  #
 
 
 if __name__ == '__main__':
